cfa5c7c75f6825376203d20163c56bc6cf6b285e
dcray863 <54968281+dcray863@users.noreply.github.com>
2019-10-01 20:42:53 -0400

    Basic Player and Camera Movement
    - Added a Player prefab.
    
    - Added a Main Camera prefab.
    
    - Added PlayerMovement Script.
    This script gives the player a constant z velocity. It allows for X and Y movement within the bounds of the screen.
    
    - Added CameraLock script.
    This script causes the main camera to follow the player's Z position at an offset

---
+++ TOBU-Casual/Assets/Scripts/CameraLock.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLock : MonoBehaviour
+{
+    [SerializeField] Transform targetPos;
+    [SerializeField] Vector3 offset = new Vector3(0f, 2.46f, -7.65f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        FollowTarget();
+    }
+
+    private void FollowTarget()
+    {
+        Vector3 newPos = new Vector3(offset.x, offset.y, targetPos.position.z + offset.z);
+
+        transform.position = newPos;
+    }
+}
