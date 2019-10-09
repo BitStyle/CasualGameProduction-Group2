26e39a87117d706bbe26b86ad03fc872579e5616
TheDavster <36806758+Dray863@users.noreply.github.com>
2019-10-09 09:35:51 -0400

    Added Tile Generator and Game Manager
    Tile Generator
    - Infinitely spawns random prefab tiles of obstacles a set distance in front of the player
    - Destroys old tiles after a limit is reached
    
    Game Manager
    - Contains game and player properties
    - Will later change Score.cs to be part of Game Manager
    
    Ring
    - Ring destroys itself on contact with player

---
+++ TOBU-Casual/Assets/Scripts/Ring.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ring : MonoBehaviour
+{
+    [SerializeField] float scoreValue = 0;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Player")
+        {
+            //Do score stuff here
+            //Do SFX stuff here
+            //Do particle FX here
+            Debug.Log("Get Ring");
+           
+            Destroy(this.gameObject);
+        }
+    }
+}
