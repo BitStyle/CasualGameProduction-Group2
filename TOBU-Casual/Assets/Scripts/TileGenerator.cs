40048198320a19497e419175a5c0f7a18c88b859
dcray863 <54968281+dcray863@users.noreply.github.com>
2019-10-21 15:20:12 -0400

    Initial World Transition Commit

--- TOBU-Casual/Assets/Scripts/TileGenerator.cs
+++ TOBU-Casual/Assets/Scripts/TileGenerator.cs
@@ -7,15 +7,22 @@ public class TileGenerator : MonoBehaviour
 {
     [SerializeField] GameObject player;
     [SerializeField] GameObject startingTile;
+    [SerializeField] GameObject spiritGate;
     [SerializeField] GameObject[] tilesArray;
+    [SerializeField] GameObject[] spiritTilesArray;
     [SerializeField] GameObject tileManager;
-    [SerializeField] float tileLength = 0;
+    [SerializeField] float tileLength = 0f;
+    [SerializeField] float timeInSpiritRealm = 5f;
     [SerializeField] int numOfStartingTiles = 1;
     [SerializeField] int tilesInAdvance = 3;
+    [SerializeField] int spiritTilesInAdvance = 6;
     [SerializeField] int maxTilesToDisplay = 6;
 
     Vector3 posToSpawn;
     int currentNumOfTiles = 0;
+    bool gateSpawned = false;
+    float spiritRealmExitTime = -1f;
+    float spiritRealmExitTimeDefault = -1f;
 
     void Start()
     {
@@ -27,8 +34,66 @@ public class TileGenerator : MonoBehaviour
 
     void Update()
     {
-        SpawnTiles();
+        if(GameManager.Instance.RingCounter >= 5)
+        {
+            if (!gateSpawned)
+            {
+                SpawnGate();
+
+                spiritRealmExitTime = Time.time + timeInSpiritRealm;
+
+                gateSpawned = true;
+            }
+            else if (gateSpawned)
+            {
+                SpawnSpiritTiles();
+            }
+        }
+        else
+        {
+            SpawnTiles();
+        }
         DestroyTiles();
+    }
+
+    private void SpawnGate()
+    {
+        GameObject gateGameObject = Instantiate(spiritGate, posToSpawn, Quaternion.identity) as GameObject;
+        gateGameObject.transform.SetParent(this.transform);
+
+        posToSpawn.z += tileLength;
+        currentNumOfTiles++;
+        Debug.Log("Gate Tile Spawned");
+    }
+
+    private void SpawnSpiritTiles()
+    {
+        float tileBuffer = tilesInAdvance * tileLength;
+        Vector3 playerPos = player.transform.position;
+        var rng = new System.Random();
+        int arrayIndex = rng.Next(1, tilesArray.Length);
+
+        //If the player's position is within 1 tile length of the tile buffer, spawn a new random tile
+        if (playerPos.z >= posToSpawn.z - tileBuffer)
+        {
+            //Instantiate the tile as a game object, and set the Tile Manager as parent
+            GameObject tileGameObject = Instantiate(spiritTilesArray[arrayIndex], posToSpawn, Quaternion.identity) as GameObject;
+            tileGameObject.transform.SetParent(this.transform);
+            Debug.Log("Tile Spawned");
+
+            //Update spawn position of next tile
+            posToSpawn.z += tileLength;
+            currentNumOfTiles++;
+        }
+
+        
+        if(Time.time >= spiritRealmExitTime)
+        {
+            gateSpawned = false;
+            GameManager.Instance.RingCounter = 0;
+            spiritRealmExitTime = spiritRealmExitTimeDefault;
+        }
+        
     }
 
     private void SpawnStartingTiles()
