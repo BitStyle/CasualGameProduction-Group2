db66185b66d3b5c1c309b6847e1acbe22d802755
David Ray <dcray863@gmail.com>
2019-11-03 19:22:45 -0500

    Ring Juice

--- TOBU-Casual/Assets/Scripts/Player.cs
+++ TOBU-Casual/Assets/Scripts/Player.cs
@@ -1,10 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] GameObject ringForEffect;
+    private IEnumerator coroutine;
+    private Vector3 originalScale;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,8 @@ public class Player : MonoBehaviour
 
         //Stop game from sleeping, because gyroscope is not a registered input.
         Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+        originalScale = ringForEffect.transform.localScale;
     }
 
     // Update is called once per frame
@@ -40,6 +47,28 @@ public class Player : MonoBehaviour
         {
             GameManager.Instance.RingCounter++;
             Debug.Log("Ring Count: " + GameManager.Instance.RingCounter);
+            RingJuice();
+        }
+    }
+
+    private void RingJuice()
+    {
+        ringForEffect.SetActive(true);
+        coroutine = LerpScaleDown(0.5f);
+        StartCoroutine(coroutine);
+    }
+
+    private IEnumerator LerpScaleDown(float time)
+    {
+        Vector3 targetScale = originalScale * 0.01f;
+        float originalTime = time;
+
+        while (time > 0.0f)
+        {
+            time -= Time.deltaTime;
+
+            ringForEffect.transform.localScale = Vector3.Lerp(targetScale, originalScale, time / originalTime);
+            yield return null;
         }
     }
 
