86cb584df024a8e3cde378bdc0edfa746da2c0dd
BitStyle <hwglaspie51@gmail.com>
2019-12-01 19:25:06 -0500

    Added load screen

---
+++ TOBU-Casual/Assets/LoadScreenTextPulse.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class LoadScreenTextPulse : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI loadText;
+    private bool faded = false;
+    [SerializeField] float duration;
+    [SerializeField] float waitTime;
+    AsyncOperation asyncLoad;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        StartCoroutine(FadeInAndOutRepeat(duration,waitTime));
+    }
+
+    private void Start()
+    {
+        StartCoroutine(LoadGameScene());
+    }
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    //Pulse alpha forever
+    IEnumerator FadeInAndOutRepeat(float duration, float waitTime)
+    {
+        WaitForSeconds waitForXSec = new WaitForSeconds(waitTime);
+
+        while (true)
+        {
+            
+            //Fade out
+            yield return fadeInAndOut(false, duration);
+
+            //Wait
+            yield return waitForXSec;
+
+            //Fade-in 
+            yield return fadeInAndOut(true, duration);
+
+        }
+    }
+
+    IEnumerator fadeInAndOut(bool fadeIn, float duration)
+    {
+        float minAlpha = 0; // min 
+        float maxAlpha = 1; // max 
+
+        float counter = 0f;
+
+        //Set Values depending on if fadeIn or fadeOut
+        float a, b;
+
+        if (fadeIn)
+        {
+            a = minAlpha;
+            b = maxAlpha;
+        }
+        else
+        {
+            a = maxAlpha;
+            b = minAlpha;
+        }
+
+        float currentAlpha = loadText.color.a;
+
+        while (counter < duration)
+        {
+            counter += Time.deltaTime;
+            Color c = loadText.color;
+            c.a = Mathf.Lerp(a, b, counter / duration);
+            loadText.color = c;
+
+            yield return null;
+        }
+    }
+
+    IEnumerator LoadGameScene()
+    {
+        asyncLoad = SceneManager.LoadSceneAsync("MainGame", LoadSceneMode.Single);
+        asyncLoad.allowSceneActivation = false;
+        asyncLoad.priority = 9;
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            loadText.text = "Loading progress: " + (asyncLoad.progress * 100) + "%";
+            yield return null;
+        }
+    }
+}
